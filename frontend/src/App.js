c7eb0dee43653570f32cf0a2af2c944783d14aef
AlainAmbrose <alainambrose@knights.ucf.edu>
2023-10-22 16:33:58 -0400

    added basic routing functionality and updated landing page UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,24 @@
+import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import {
+  BrowserRouter as Router,
+  Route,
+  Navigate,
+  Routes,
+} from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <LoginPage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/signup" element={<SignUpPage />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
