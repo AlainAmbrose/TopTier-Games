442e812be353910bb53c4895ba692b38f7b14083
Nelson Herrera <ndanielherrera@icloud.com>
2023-10-24 11:00:17 -0400

    Added Login Background image and Title to login and signup landing page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,18 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    fontSize: {
+      '4xl': ['1.8rem', {
+        lineHeight: '2rem',
+        letterSpacing: '-0.01em',
+        fontWeight: '500',
+      }],
+      '9xl': ['7rem', {
+        lineHeight: '2.25rem',
+        letterSpacing: '-0.02em',
+        fontWeight: '700',
+      }],
+    }
   },
   plugins: [],
 }
