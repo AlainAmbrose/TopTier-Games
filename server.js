7070e66158aaf78e7073cb5311c43889dd1cfd54
Nelson Herrera <ndanielherrera@icloud.com>
2023-10-27 12:53:32 -0400

    Handled Local Frontend-004 Merge and updated local port for server to 5000 instead of 3000

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 
 require("dotenv").config();
 const path = require("path");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const uri = process.env.MONGODB_URL;
 
 const usersRouter = require("./controllers/Users");
