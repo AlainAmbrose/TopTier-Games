d704b8f4642d93a54194b58ddfd33ef7743ca3c8
Nelson Herrera <ndanielherrera@icloud.com>
2023-11-01 11:41:41 -0600

    Fixed Small bug with First and Last name on homepage

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -526,7 +526,7 @@ const HomePage = () => {
                         className="ml-4 text-sm font-semibold leading-6 text-gray-200"
                         aria-hidden="true"
                       >
-                        {firstName + " " + lastName}
+                        {/* {firstName + " " + lastName} */}
                       </span>
                       <ChevronDownIcon
                         className="ml-2 h-5 w-5 text-gray-400"
