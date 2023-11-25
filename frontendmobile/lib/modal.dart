ad8ed8e85083fb23fcc0ec63f3fa8f598e9750c1
Ethan Frakes <91703980+thefrostedfrakes@users.noreply.github.com>
2023-11-25 15:43:02 -0500

    Genres, ESRB rating and reviews added to modal, extra formatting changes

--- frontendmobile/lib/modal.dart
+++ frontendmobile/lib/modal.dart
@@ -4,20 +4,97 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
 class Modal {
+  Icon _setRatingStars(int index, String ratingStr) {
+    IconData iconSymbol;
+    double rating = double.parse(ratingStr);
+
+    if (index < rating.floor() || rating % 1.0 > 0.7) {
+      iconSymbol = Icons.star;
+    }
+    else if (rating % 1.0 > 0.4 && rating % 1.0 < 0.7) {
+      iconSymbol = Icons.star_half_outlined;
+    }
+    else {
+      iconSymbol = Icons.star_border;
+    }
+    return Icon(
+        iconSymbol,
+        color: Colors.amber,
+    );
+  }
+
   String _generatePlatformString(List platforms) {
     String platformStr = (platforms.length == 1) ? "Platform: " : "Platforms: ";
 
     for (int i = 0; i < platforms.length; i++) {
-        platformStr += (i < platforms.length-1) ? platforms[i] + ', ' : platforms[i];
+        platformStr += (i < platforms.length-1)
+            ? '${platforms[i]}, '
+            : '${platforms[i]}\n';
     }
 
     return platformStr;
   }
 
+  String _generateGenreString(List genres) {
+    String genreStr = (genres.length == 1) ? "Genre: " : "Genres: ";
+
+    Map<int, String> genreMap = {
+      2: 'Point-and-click',
+      4: 'Fighting',
+      5: 'Shooter',
+      7: 'Music',
+      8: 'Platform',
+      9: 'Puzzle',
+      10: 'Racing',
+      11: 'Real Time Strategy (RTS)',
+      12: 'Role-playing (RPG)',
+      13: 'Simulator',
+      14: 'Sport',
+      15: 'Strategy',
+      16: 'Turn-based strategy (TBS)',
+      24: 'Tactical',
+      25: 'Hack and slash/Beat \'em up',
+      26: 'Quiz/Trivia',
+      30: 'Pinball',
+      31: 'Adventure',
+      32: 'Indie',
+      33: 'Arcade',
+      34: 'Visual Novel',
+      35: 'Card & Board Game',
+      36: 'MOBA',
+    };
+
+    for (int i = 0; i < genres.length; i++) {
+      genreStr += ((i < genres.length-1)
+          ? '${genreMap[genres[i]]!}, '
+          : '${genreMap[genres[i]]}\n')!;
+    }
+
+    return genreStr;
+  }
+
   String _formatDate(String releaseDate) {
     DateTime dt = DateTime.parse(releaseDate);
 
-    return "${dt.month}/${dt.day}/${dt.year}";
+    return "${dt.month}/${dt.day}/${dt.year}\n";
+  }
+
+  String _setESRBRating(dynamic ageRating) {
+    if (ageRating.runtimeType != List || !ageRating[0].containsKey('rating')) {
+      return "ESRB: N/A";
+    }
+
+    Map<int, String> esrbRating = {
+      6: 'RP',
+      7: 'EC',
+      8: 'E',
+      9: 'E10',
+      10: 'T',
+      11: 'M',
+      12: 'AO',
+    };
+
+    return "ESRB: ${esrbRating[ageRating[0]['rating']]!}";
   }
 
   void _addToLibrary(BuildContext context,
@@ -119,23 +196,28 @@ class Modal {
               Container(
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(20.0),
-                  color: const Color(0xD0263238),
+                  color: Colors.grey[900]!.withOpacity(0.85),
                 ),
+                alignment: Alignment.center,
                 padding: const EdgeInsets.all(20.0),
-                width: MediaQuery.of(context).size.width * 0.9,
+                width: MediaQuery.of(context).size.width,
                 child: Column(
                   mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(game['Name'],
+                    Align(
+                      alignment: Alignment.center,
+                      child:
+                        Text(game['Name'],
                           style: const TextStyle(
                               color: Colors.white,
                               fontWeight: FontWeight.bold,
                               fontStyle: FontStyle.italic,
                               fontSize: 24.0
                           ),
-                        textAlign: TextAlign.center,
+                          textAlign: TextAlign.center,
+                        ),
                     ),
                     Container(
                         margin: const EdgeInsets.symmetric(vertical: 8.0),
@@ -152,19 +234,53 @@ class Modal {
                         height: MediaQuery.of(context).size.width,
                       ),
                     ),
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: List.generate(
+                              5,
+                                (index) => _setRatingStars(index, game['GameRanking']['\$numberDecimal']),
+                            ),
+                          ),
+                          const SizedBox(width: 10.0),
+                          Text(
+                            "${double.parse(game['GameRanking']['\$numberDecimal']).toStringAsFixed(2)}   ${game['ReviewCount']} Reviews",
+                            style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 15.0
+                            ),
+                          )
+                        ]
+                    ),
                     Text(_generatePlatformString(game['Platforms']),
                           style: const TextStyle(
                               color: Colors.white,
                               fontSize: 15.0
                           ),
-                        textAlign: TextAlign.center,
+                        textAlign: TextAlign.left,
+                    ),
+                    Text(_generateGenreString(game['Genre']),
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 15.0
+                          ),
+                        textAlign: TextAlign.left,
                     ),
                     Text("Release date: ${_formatDate(game['ReleaseDate'])}",
                             style: const TextStyle(
                                 color: Colors.white,
                                 fontSize: 15.0
                             ),
-                          textAlign: TextAlign.center,
+                          textAlign: TextAlign.left,
+                    ),
+                    Text(_setESRBRating(game['AgeRating']),
+                      style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 15.0
+                      ),
+                      textAlign: TextAlign.left,
                     ),
                     const Text("\nDescription:",
                         style: TextStyle(
@@ -173,7 +289,7 @@ class Modal {
                             fontStyle: FontStyle.italic,
                             fontSize: 18.0
                         ),
-                        textAlign: TextAlign.center,
+                        textAlign: TextAlign.left,
                     ),
                     Text((game['Summary'] != null)
                         ? "${game['Summary']}"
@@ -182,22 +298,25 @@ class Modal {
                               color: Colors.white,
                               fontSize: 15.0
                           ),
-                        textAlign: TextAlign.center,
+                        textAlign: TextAlign.left,
                     ),
-                      ElevatedButton(
-                          onPressed: () {
-                            _addToLibrary(context, game, userInfo);
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.green,
-                            foregroundColor: Colors.black,
-                            padding: const EdgeInsets.symmetric(horizontal: 75),
-                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                          ),
-                          child: const Text('Add to Library', style: TextStyle(
-                              color: Colors.white)
-                          )
-                      )
+                      Align(
+                        alignment: Alignment.center,
+                        child: ElevatedButton(
+                            onPressed: () {
+                              _addToLibrary(context, game, userInfo);
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.green,
+                              foregroundColor: Colors.black,
+                              padding: const EdgeInsets.symmetric(horizontal: 75),
+                              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                            ),
+                            child: const Text('Add to Library', style: TextStyle(
+                                color: Colors.white)
+                            )
+                        )
+                      ),
                   ]
               )
           ),
