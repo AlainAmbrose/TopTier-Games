42c2d749ed3487994dd0f445470eeeeee5049f38
KimYe9 <ye.m.kim3@gmail.com>
2023-11-27 06:13:30 -0500

    Library list populates games, added delete and edit options

--- frontendmobile/lib/library.dart
+++ frontendmobile/lib/library.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontendmobile/modal.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'appbar.dart';
@@ -12,20 +13,32 @@ class LibraryPage extends StatefulWidget {
   State<LibraryPage> createState() => _LibraryPageState();
 }
 
-class _LibraryPageState extends State<LibraryPage> {
+class _LibraryPageState extends State<LibraryPage>
+    with TickerProviderStateMixin {
   List<Map<String, dynamic>>? userGamesList;
   List<Map<String, dynamic>>? filteredGames;
+  late TabController _tabController;
+  Map<String, dynamic> ? gameInfo;
   bool _mounted = true;
 
   @override
   void initState() {
     _loadUserList(0);
+    _tabController = TabController(length: 3, initialIndex: 0, vsync: this);
+    // Here is the addListener!
+    _tabController.addListener(_handleTabSelection);
     super.initState();
+  }
+
+  void _handleTabSelection() {
+    if (_tabController.indexIsChanging) {
+      _loadUserList(_tabController.index);
+    }
   }
 
   @override
   void dispose() {
-    _mounted = false;
+    //_mounted = false;
     super.dispose();
   }
 
@@ -51,6 +64,58 @@ class _LibraryPageState extends State<LibraryPage> {
     return 'https:${json.decode(response.body)['url']}';
   }
 
+  Future _getGameInfo(int gameId) async {
+    // ... existing code
+    var jsonSendData = {
+      'gameId': gameId,
+      'options': {
+        '_id': true,
+        'id': true,
+        'name': true,
+        'coverURL': true,
+        'storyline': true,
+        'releasedate': true,
+        'genres': true,
+        'gameranking': true,
+        'images': true,
+        'links': true,
+        'platforms': true,
+        'platformlogos': true,
+        'videos': true,
+        'ageratings': true,
+        'similargames': true,
+        'reviewcount': true
+      }
+    };
+
+    final jsonData = jsonEncode(jsonSendData);
+
+    final headers = <String, String>{
+      'Content-Type': 'application/json',
+      'authorization': '${widget.jsonResponse['accessToken']}',
+      'Cookie': 'jwt_access=${widget.jsonResponse['accessToken']}',
+    };
+
+    var response = await http.post(
+      Uri.parse('https://www.toptier.games/Games/api/getgameinfo'),
+      headers: headers,
+      body: jsonData,
+    );
+
+    if (response.statusCode == 200) {
+      setState(() {
+        var data = jsonDecode(response.body);
+        print("data: $data");
+        setState(() {
+          gameInfo = data['gameInfo'];
+        });
+      });
+
+    } else {
+      print("Status Code");
+      print(response.statusCode);
+    }
+  }
   void _loadUserList(int tabNumber) async {
     Map<String, dynamic> jsonSendData = <String, dynamic>{};
 
@@ -81,10 +146,12 @@ class _LibraryPageState extends State<LibraryPage> {
             userGamesList =
                 (data['games'] as List<dynamic>).cast<Map<String, dynamic>>();
             filteredGames = userGamesList
-                ?.where((userGame) => userGame['Status'] == tabNumber)
+                ?.where((userGamesList) => userGamesList['Status'] == tabNumber)
                 .toList();
+            print("$tabNumber");
             print("filteredGames: ${filteredGames}");
-            print(filteredGames);
+
+            //print(filteredGames);
           });
         } else {
           print('Error: ${response.statusCode}');
@@ -110,7 +177,7 @@ class _LibraryPageState extends State<LibraryPage> {
               child: ListTile(
                 title: Center(
                   child: Text(
-                    'No games in your library',
+                    'No games in this list',
                     textAlign: TextAlign.center,
                     style: TextStyle(
                         color: Colors.white,
@@ -128,28 +195,138 @@ class _LibraryPageState extends State<LibraryPage> {
       return ListView.builder(
           itemCount: filteredGames?.length,
           itemBuilder: (BuildContext context, int index) {
-            print('filteredGames length: ${filteredGames?.length}');
-            print('Current index: $index');
+            //print('filteredGames length: ${filteredGames?.length}');
+            //print('Current index: $index');
             return SizedBox(
-              height:200,
-              child: Row(
-                  children: <Widget>[
-                    Container(
-                      padding: const EdgeInsets.all(13.0),
-                      height: 170.0,
-                      width: 130,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(4.0),
-                        child: Image.network(
-                          'https:${filteredGames?[index]['url']}',
-                          height: 170.0,
-                          width: 115,
-                          fit: BoxFit.fill,
+              height: 180,
+              child: GestureDetector(
+                onTap: () async {
+                  await _getGameInfo(filteredGames?[index]['id']);
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      print(filteredGames?[index]['id']);
+                      return LibraryModal().returnModal(context, gameInfo ?? {}, gameInfo?['url'], widget.jsonResponse);
+                    },
+                  );
+                },
+                child: Card(
+                  color: Colors.transparent,
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        padding: const EdgeInsets.only(
+                            left: 12.0, bottom: 10.0, top: 10.0),
+                        height: 175.0,
+                        width: 130,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(4.0),
+                          child: Image.network(
+                            'https:${filteredGames?[index]['url']}',
+                            height: 160.0,
+                            width: 115,
+                            fit: BoxFit.fill,
+                          ),
                         ),
                       ),
-                    ),
-
-                  ],
+                      Expanded(
+                        child: Column(
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.only(
+                                  left: 12.0, bottom: 25.0, top: 10.0),
+                              alignment: Alignment.topLeft,
+                              child: Text(
+                                '${filteredGames?[index]['name']}',
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: 'Inter-Bold',
+                                  fontStyle: FontStyle.italic,
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 26,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              padding: const EdgeInsets.only(
+                                  left: 12.0, bottom: 10.0),
+                              alignment: Alignment.topLeft,
+                              child: Text(
+                                'Hours Played: ${filteredGames?[index]['HoursPlayed']}',
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: 'Inter-Bold',
+                                  fontStyle: FontStyle.italic,
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 15,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              padding: const EdgeInsets.only(
+                                  left: 12.0, bottom: 10.0),
+                              alignment: Alignment.topLeft,
+                              child: Text(
+                                'Your Rating: $index',
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: 'Inter-Bold',
+                                  fontStyle: FontStyle.italic,
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 15,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      Align(
+                          alignment: Alignment.topRight,
+                          child: PopupMenuButton<String>(
+                            icon: const Icon(Icons.more_vert),
+                            onSelected: (value) {
+                              if (value == 'Remove Game') {
+                                showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return DeleteModal().returnModal(
+                                          context, filteredGames![index]);
+                                    });
+                              } else if (value == 'Edit Game') {}
+                            },
+                            color: Colors.grey[800]!.withOpacity(0.95),
+                            itemBuilder: (BuildContext context) {
+                              return [
+                                const PopupMenuItem<String>(
+                                  value: 'Edit Game',
+                                  child: Text(
+                                    'Edit Game',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontFamily: 'Inter-Light',
+                                      fontWeight: FontWeight.w300,
+                                      fontStyle: FontStyle.italic,
+                                    ),
+                                  ),
+                                ),
+                                const PopupMenuItem<String>(
+                                  value: 'Remove Game',
+                                  child: Text(
+                                    'Remove Game',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontFamily: 'Inter-Light',
+                                      fontWeight: FontWeight.w300,
+                                      fontStyle: FontStyle.italic,
+                                    ),
+                                  ),
+                                ),
+                              ];
+                            },
+                          ))
+                    ],
+                  ),
+                ),
               ),
             );
           });
@@ -194,6 +371,7 @@ class _LibraryPageState extends State<LibraryPage> {
                     borderRadius: BorderRadius.circular(50),
                   ),
                   child: TabBar(
+                    controller: _tabController,
                     unselectedLabelStyle: const TextStyle(
                         fontFamily: 'Inter-Regular',
                         fontStyle: FontStyle.italic,
@@ -220,7 +398,7 @@ class _LibraryPageState extends State<LibraryPage> {
                         text: "Playing",
                       ),
                       Tab(
-                        text: "Complete",
+                        text: "Completed",
                       ),
                     ],
                   )),
--- frontendmobile/lib/modal.dart
+++ frontendmobile/lib/modal.dart
@@ -327,3 +327,385 @@ class Modal {
     );
   }
 }
+
+class DeleteModal{
+
+  Dialog returnModal(BuildContext context,
+      Map<String, dynamic> game) {
+    return Dialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        backgroundColor: Colors.transparent,
+        child: Container(
+          padding:const EdgeInsets.all(20.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20.0),
+            color: Colors.grey[900]!.withOpacity(0.85),
+          ),
+          height: 400,
+          child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text('Are you sure you want to delete:',
+                  textAlign: TextAlign.center,
+                  style:  TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Inter-Light',
+                      fontWeight: FontWeight.w300,
+                      fontStyle: FontStyle.italic,
+                      fontSize: 25
+                  ),
+                ),
+              ),
+                Text('${game['name']}',
+                  style:  const TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Inter-Bold',
+                      fontWeight: FontWeight.w700,
+                      fontStyle: FontStyle.italic,
+                      fontSize: 25
+                ),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(5.0),
+                  child: Image.network(
+                    'https:${game['url']}',
+                    height: 160.0,
+                    width: 115,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+              const Text('from your library?',
+                style:  TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Inter-Light',
+                  fontWeight: FontWeight.w300,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 25
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                      //padding: const EdgeInsets.all(10.0),
+                      child:Padding(
+                        padding: const EdgeInsets.only(right: 6.0, top: 7.0),
+                  child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.grey,
+                        foregroundColor: Colors.black,
+                        padding: const EdgeInsets.all(5.0),
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
+                      ),
+                      child: const Text('No', style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Inter-Light',
+                        fontWeight: FontWeight.w300,
+                        fontStyle: FontStyle.italic,
+                      )
+                      )
+                  )),
+              ),
+                 Expanded(
+                   child: Padding(
+                     padding: const EdgeInsets.only(left: 6.0, top: 7.0),
+                     child:ElevatedButton(
+                         onPressed: () {
+
+                         },
+                         style: ElevatedButton.styleFrom(
+                           backgroundColor: Colors.green,
+                           foregroundColor: Colors.black,
+                           padding: const EdgeInsets.all(5.0),
+                           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
+                         ),
+                         child: const Text('Yes', style: TextStyle(
+                           color: Colors.white,
+                           fontFamily: 'Inter-Light',
+                           fontWeight: FontWeight.w300,
+                           fontStyle: FontStyle.italic,
+                         )
+                         )
+                     ),
+                   )
+
+                 )
+                ],
+              )
+            ],
+          ),
+        )
+    );
+  }
+}
+
+class LibraryModal {
+  String _generatePlatformString(List platforms) {
+    String platformStr = (platforms.length == 1) ? "Platform: " : "Platforms: ";
+
+    for (int i = 0; i < platforms.length; i++) {
+      platformStr += (i < platforms.length-1)
+          ? '${platforms[i]}, '
+          : '${platforms[i]}\n';
+    }
+
+    return platformStr;
+  }
+
+  String _generateGenreString(List genres) {
+    String genreStr = (genres.length == 1) ? "Genre: " : "Genres: ";
+
+    Map<int, String> genreMap = {
+      2: 'Point-and-click',
+      4: 'Fighting',
+      5: 'Shooter',
+      7: 'Music',
+      8: 'Platform',
+      9: 'Puzzle',
+      10: 'Racing',
+      11: 'Real Time Strategy (RTS)',
+      12: 'Role-playing (RPG)',
+      13: 'Simulator',
+      14: 'Sport',
+      15: 'Strategy',
+      16: 'Turn-based strategy (TBS)',
+      24: 'Tactical',
+      25: 'Hack and slash/Beat \'em up',
+      26: 'Quiz/Trivia',
+      30: 'Pinball',
+      31: 'Adventure',
+      32: 'Indie',
+      33: 'Arcade',
+      34: 'Visual Novel',
+      35: 'Card & Board Game',
+      36: 'MOBA',
+    };
+
+    for (int i = 0; i < genres.length; i++) {
+      genreStr += ((i < genres.length-1)
+          ? '${genreMap[genres[i]]!}, '
+          : '${genreMap[genres[i]]}\n')!;
+    }
+
+    return genreStr;
+  }
+
+  String _formatDate(String releaseDate) {
+    DateTime dt = DateTime.parse(releaseDate);
+
+    return "${dt.month}/${dt.day}/${dt.year}\n";
+  }
+
+  String _setESRBRating(dynamic ageRating) {
+    if (ageRating.runtimeType != List ||
+        ageRating.length == 0 ||
+        !ageRating[0].containsKey('rating')) {
+      return "ESRB: N/A";
+    }
+
+    Map<int, String> esrbRating = {
+      6: 'RP',
+      7: 'EC',
+      8: 'E',
+      9: 'E10',
+      10: 'T',
+      11: 'M',
+      12: 'AO',
+    };
+
+    return "ESRB: ${esrbRating[ageRating[0]['rating']]!}";
+  }
+
+
+  Dialog returnModal(BuildContext context,
+      Map<String, dynamic> game,
+      String coverUrl,
+      Map<String, dynamic> userInfo) {
+    return Dialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        backgroundColor: Colors.transparent,
+        child: SingleChildScrollView(
+            child: Stack(
+                children: [
+                  Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20.0),
+                        color: Colors.grey[900]!.withOpacity(0.85),
+                      ),
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.all(20.0),
+                      width: MediaQuery.of(context).size.width,
+                      child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Align(
+                              alignment: Alignment.center,
+                              child:
+                              Text(game['name'],
+                                style: const TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontStyle: FontStyle.italic,
+                                    fontSize: 24.0
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                            Container(
+                              margin: const EdgeInsets.symmetric(vertical: 8.0),
+                              decoration: BoxDecoration(
+                                  color: Colors.grey,
+                                  borderRadius: BorderRadius.circular(15.0),
+                                  border: Border.all(width: 2.0, color: Colors.white),
+                                  image: DecorationImage(
+                                      image: NetworkImage("https:$coverUrl"),
+                                      fit: BoxFit.fill
+                                  )
+                              ),
+                              constraints: BoxConstraints.expand(
+                                height: MediaQuery.of(context).size.width,
+                              ),
+                            ),
+                            const Text("Status: ",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontStyle: FontStyle.italic,
+                                    fontSize: 24.0
+                                ),
+                                textAlign: TextAlign.left),
+                            const Text("Your Rating:",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontStyle: FontStyle.italic,
+                                  fontSize: 24.0
+                              ),
+                              textAlign: TextAlign.left),
+
+                            const Text("Hours Played: ",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontStyle: FontStyle.italic,
+                                    fontSize: 24.0
+                                ),
+                            ),
+                            const SizedBox(height:15.0),
+
+                            Text(_generatePlatformString(game['platforms']),
+                              style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15.0
+                              ),
+                              textAlign: TextAlign.left,
+                            ),
+                            Text(_generateGenreString(game['genres']),
+                              style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15.0
+                              ),
+                              textAlign: TextAlign.left,
+                            ),
+                            Text("Release date: ${_formatDate(game['releasedate'])}",
+                              style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15.0
+                              ),
+                              textAlign: TextAlign.left,
+                            )
+                          ]
+                      )
+                  ),
+                ])
+        )
+    );
+  }
+}
+
+class EditModal {
+  final TextEditingController _ratingController = TextEditingController();
+  final TextEditingController _reviewController = TextEditingController();
+
+  Dialog returnModal(BuildContext context,
+      Map<String, dynamic> game,
+      String coverUrl,
+      Map<String, dynamic> userInfo) {
+    return Dialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        backgroundColor: Colors.transparent,
+        child: SingleChildScrollView(
+            child: Stack(
+                children: [
+                  Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20.0),
+                        color: Colors.grey[900]!.withOpacity(0.85),
+                      ),
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.all(20.0),
+                      width: MediaQuery
+                          .of(context)
+                          .size
+                          .width,
+                      child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Align(
+                              alignment: Alignment.center,
+                              child:
+                              Text(game['name'],
+                                style: const TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontStyle: FontStyle.italic,
+                                    fontSize: 24.0
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                            Container(
+                              margin: const EdgeInsets.symmetric(vertical: 8.0),
+                              decoration: BoxDecoration(
+                                  color: Colors.grey,
+                                  borderRadius: BorderRadius.circular(15.0),
+                                  border: Border.all(
+                                      width: 2.0, color: Colors.white),
+                                  image: DecorationImage(
+                                      image: NetworkImage("https:$coverUrl"),
+                                      fit: BoxFit.fill
+                                  )
+                              ),
+                              constraints: BoxConstraints.expand(
+                                height: MediaQuery
+                                    .of(context)
+                                    .size
+                                    .width,
+                              ),
+                            ),
+
+                          ]
+                      )
+                  ),
+                ])
+        )
+    );
+  }
+}
